e05e05fb4294c81a4f12e81063525c632a6475cf
Railton98 <railtonleal98@gmail.com>
2019-02-22 23:43:43 -0300

    iniciando com rotas

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,14 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-new Tecks\Framework\Router;
-new App\Module;
+$router = new Tecks\Framework\Router;
+
+$router->add('/', function () {
+    return 'estamos na homepage';
+});
+
+$router->add('/projects', function () {
+    return 'estamos listando projetos';
+});
+
+echo $router->run();
--- src/Router.php
+++ src/Router.php
@@ -7,8 +7,20 @@ namespace Tecks\Framework;
  */
 class Router
 {
-	public function __construct()
+	private $routes = [];
+
+    public function add(String $pattern, $callback)
     {
-        echo "<h1>Olá!</h1>";
+        $this->routes[$pattern] = $callback;
+    }
+
+    public function run()
+    {
+        $route = $_SERVER['PATH_INFO'] ?? '/';
+
+        if (array_key_exists($route, $this->routes)) {
+            return $this->routes[$route]();
+        }
+        return 'Página não encontrada!';
     }
 }
